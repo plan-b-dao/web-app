4e6bb4c0d8a209b0cc09ef437b4369ce8e5aeade
Edin Hadzovic <contact@edinhadzovic.com>
2022-07-25 19:56:00 +0200

    adding new route called my circle.

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,7 @@ export const config = {
     isFounderActive: process.env.REACT_APP_FOUNDER_ACTIVE === "1" ? true : false,
     getNavList: () => {
         const supportedRoutes = process.env.REACT_APP_SUPPORTED_ROUTES?.split(", ");
-        const routes = ["overview", "wallet", "market", "dao", "swap", "my-cards", "my-rewards", "market-checker", "seed-capital"]
+        const routes = ["overview", "wallet", "my-circle", "market", "dao", "swap", "my-cards", "my-rewards", "market-checker", "seed-capital"]
         return routes.map((route) => {
             const findRoute = supportedRoutes!.find((sr) => sr === route);
 
